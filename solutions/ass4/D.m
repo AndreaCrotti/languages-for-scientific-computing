c003eab7adce23209ad5c4fbb119faca95d3cfab
Andrea Crotti <kerny404@gmail.com>
2009-11-29 16:19:00 +0100

    indentation

--- solutions/ass4/D.m
+++ solutions/ass4/D.m
@@ -1,11 +1,16 @@
 function M = D (k, n)
 
-D = [];
-for j = 0 : n - 1
-	D (j + 1, j + 1) = exp (-2 * pi * i * j / n / k);
-end
+  D = [];
+  for j = 0 : n - 1
+    D (j + 1, j + 1) = exp (-2 * pi * i * j / n / k);
+  endfor
+    
+  M = [];
+  for a = 0 : k - 1
+    M (1 + n * a : n * (a + 1), 1 + n * a : n * (a + 1)) = D ^ a;
+  endfor
 
-M = [];
-for a = 0 : k - 1
-	M (1 + n * a : n * (a + 1), 1 + n * a : n * (a + 1)) = D ^ a;
-end
+endfunction
+
+    
+      

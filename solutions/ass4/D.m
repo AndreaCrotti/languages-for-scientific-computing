97c5a8cf9a428494ddb27570e67061d1db2bfd9e
eus <eus@eus-laptop.(none)>
2009-11-29 14:19:08 +0100

    Adding function D

---
+++ solutions/ass4/D.m
@@ -0,0 +1,11 @@
+function M = D (k, n)
+
+D = [];
+for j = 0 : n - 1
+	D (j + 1, j + 1) = exp (-2 * pi * i * j / n / k);
+end
+
+M = [];
+for a = 0 : k - 1
+	M (1 + n * a : n * (a + 1), 1 + n * a : n * (a + 1)) = D ^ a;
+end

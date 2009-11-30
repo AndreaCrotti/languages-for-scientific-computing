c478ca7bd9cddb5adbf2fc5d3c27683f91e0f414
Andrea Crotti <kerny404@gmail.com>
2009-11-30 18:55:36 +0100

    changing letter D -> F

--- solutions/ass4/D.m
+++ solutions/ass4/D.m
@@ -1,16 +1,17 @@
+############################
+# Twiddle factor function  #
+############################
+
 function M = D (k, n)
 
-  D = [];
+  F = [];
   for j = 0 : n - 1
-    D (j + 1, j + 1) = exp (-2 * pi * i * j / n / k);
+    F (j + 1, j + 1) = exp (-2 * pi * i * j / n / k);
   endfor
     
   M = [];
   for a = 0 : k - 1
-    M (1 + n * a : n * (a + 1), 1 + n * a : n * (a + 1)) = D ^ a;
+    M (1 + n * a : n * (a + 1), 1 + n * a : n * (a + 1)) = F ^ a;
   endfor
 
 endfunction
-
-    
-      

6565b423e2a2f3472643b195a306c23670b17946
eus <eus@eus-laptop.(none)>
2009-12-02 23:25:39 +0100

    Committing the draft of the solution to the first problem

--- solutions/ass4/MF.m
+++ solutions/ass4/MF.m
@@ -1,12 +1,67 @@
-##########################################
-# Our own implementation of FFT function #
-##########################################
+% X is a square matrix
+function Y = F2 (X)
 
-function Y = MF(n, m)
-  # Im/2 is 
-  Im = eye(m  / 2)
-  In = eye(n / 2)
+k = 2;
+n = rows (X) / k;
 
-  P(m, m/2) * (kron(eye(2), F(m/2))) * D(2, m/2) * ()
-  
-endfunction
+% Partitioning
+Xtl = X (    1 : n  , 1 : n); Xtr = X (    1 : n  ,     n : end);
+Xbl = X (n + 1 : end, 1 : n); Xbr = X (n + 1 : end, n + 1 : end);
+
+% Multiplicating with I on the left
+for r = 1 : n
+    for c = 1 : n
+	Ytl (r, c) = Xtl (r, c) + Xbl (r, c); Ytr (r, c) = Xtr (r, c) + Xbr (r, c);
+	Ybl (r, c) = Xtl (r, c) - Xbl (r, c); Ybr (r, c) = Xtr (r, c) - Xbr (r, c);
+    end
+end
+
+% Multiplicating with I on the right
+for r = 1 : n
+    for c = 1 : n
+	Xtl (r, c) = Ytl (r, c) + Ytr (r, c); Xtr (r, c) = Ytl (r, c) - Ytr (r, c);
+	Xbl (r, c) = Ybl (r, c) + Ybr (r, c); Xbr (r, c) = Ybl (r, c) - Ybr (r, c);
+    end
+end
+
+d = D (k, n);
+% Multiplicating with D on the left and right
+for r = 1 : n
+    for c = 1 : n
+	Ytl (r, c) =         d (r, r) * Xtl (r, c) * d (r, r); Ytr (r, c) =         d (r, r) * Xtr (r, c) * d (r + n, r + n);
+	Ybl (r, c) = d (r + n, r + n) * Xbl (r, c) * d (r, r); Ybr (r, c) = d (r + n, r + n) * Xbr (r, c) * d (r + n, r + n);
+    end
+end
+
+KP = kron (eye (k), F (n));
+KP_tl = KP (    1 : n  ,     1 : n);
+KP_br = KP (n + 1 : end, n + 1 : end);
+% Multiplicating with Kronecker product on the left
+Xtl = KP_tl * Ytl; Xtr = KP_tl * Ytr;
+Xbl = KP_br * Ybl; Xbr = KP_br * Ybr;
+
+% Multiplicating with Kronecker product on the right
+Xtl = Xtl * KP_tl; Xtr = Xtr * KP_br;
+Xbl = Xbl * KP_tl; Xbr = Xbr * KP_br;
+
+% Multiplicating with the permutation matrix on the left
+for i = 1 : k
+    for j = i : k : n * k
+	if (j <= n)
+	    Ytl (j    , :) = Xtl (j, :)    ; Ytr (j    , :) = Xtr (j    , :);
+	else
+	    Ybl (j % n, :) = Xbl (j % n, :); Ybr (j % n, :) = Xbr (j % n, :);
+	end
+    end
+end
+
+% Multiplicating with the transpose of the permutation matrix on the right
+for i = 1 : k
+    for j = i : k : n * k
+	if (j <= n)
+	    Xtl (:, j    ) = Ytl (:, j)    ; Xtr (:, j    ) = Ytr (:, j    );
+	else
+	    Xbl (:, j % n) = Ybl (:, j % n); Xbr (:, j % n) = Ybr (:, j % n);
+	end
+    end
+end

ecfc52e32cc5adda192738a7a7125aa8f49478ca
Andrea Crotti <kerny404@gmail.com>
2009-12-02 15:35:55 +0100

    adding MF (my fft file)

---
+++ solutions/ass4/MF.m
@@ -0,0 +1,12 @@
+##########################################
+# Our own implementation of FFT function #
+##########################################
+
+function Y = MF(n, m)
+  # Im/2 is 
+  Im = eye(m  / 2)
+  In = eye(n / 2)
+
+  P(m, m/2) * (kron(eye(2), F(m/2))) * D(2, m/2) * ()
+  
+endfunction

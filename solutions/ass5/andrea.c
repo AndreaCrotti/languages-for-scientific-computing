5b942ac9f34810fe42ac59c79045f0393e5d253e
Andrea Crotti <kerny404@gmail.com>
2010-01-02 16:50:51 +0100

    generate triangular matrix

--- solutions/ass5/andrea.c
+++ solutions/ass5/andrea.c
@@ -1,9 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include "utils.h"
+
+double *gen_L(int);
 
 int main(int argc, char *argv[])
 {
+  // initialization of ithe seed is done once for all
+  srand48( (unsigned)time((time_t *) NULL )); 
+  double *L;
   
+  L = gen_L(5);
+    
   return 0;
 }
 
+// must be equivalent to rand(n) + n * eye(n)
+double *gen_L(int len) {
+  int i, j, pos;
+  int tri_len = (len * (len + 1)) / 2; // using Gauss formula to allocate as least as possible
+  double *L = (double *) malloc(sizeof(double) * tri_len);
+  
+  pos = 0;
+  for (i = 0; i < len; i++) {
+    for (j = 0; j <= i; j++) {
+      //printf("pos = %d\n", pos);
+      L[pos] = drand48();
+      if (i == j)
+	L[pos] += len;
+      printf("%.4f\t", L[pos]);
+      pos++;
+    }
+    printf("\n");
+  }
+  return L;
+}
--- solutions/ass5/trsv.py
+++ solutions/ass5/trsv.py
@@ -8,6 +8,8 @@ for prototyping and testing
 import numpy as np
 import random
 
+from pprint import pprint
+
 def gen_matrix(len):
     matrix = [[random.random() for _ in range(len)] for _ in range(len)]
     for i in range(len):
@@ -17,4 +19,30 @@ def gen_matrix(len):
 def gen_vector(len):
     return [random.random() for _ in range(len)]
 
-mat = np.matrix(gen_matrix(10))
+# Use numpy whenever possible
+# mat = np.matrix(gen_matrix(10))
+
+def solve(L, y):
+    "Return x such that L*x = y where L is triangular"
+    x = [None]*len(L)
+    for i in range(len(x)):
+        before = sum(L[i][j]*x[j] for j in range(i))
+        x[i] = float(y[i] - before) / L[i][i]
+    return x
+
+def gen_tri_matrix(len):
+    "Generates a random triangular matrix of size len*len"
+    return [[random.randrange(1,100) for i in range(j)] for j in range(1, len+1)]
+
+def gen_int_vector(len):
+    return [random.randrange(1, 10) for i in range(len)]
+
+tri_l = [[1, 0, 0], [2, 1, 0], [3, 2, 1]]
+tri_y = [1, 1, 1]
+
+print solve(tri_l, tri_y)
+
+rtri = gen_tri_matrix(100)
+vtri = gen_int_vector(100)
+print rtri
+pprint(solve(rtri, vtri))

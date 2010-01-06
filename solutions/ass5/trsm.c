f599de16ffd38b09933b0f201d23732dd7fca935
Andrea Crotti <kerny404@gmail.com>
2010-01-06 14:40:19 +0100

    args for trsv

--- solutions/ass5/trsm.c
+++ solutions/ass5/trsm.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> // for memcpy
 #include <time.h> // for timing the execution
 #include "utils.h"
 
 #define TS ((double) clock())/CLOCKS_PER_SEC
 #define CYCLES (int) pow(10, 1000)
 #define DEBUG 1
+
+// TODO: compile BLAS first
 
 // Solve Lx=y using blas functions
 double *trsm(double *, double *, int, int);
@@ -14,12 +17,13 @@ double pow(double, double);
 // this should be the definition from common_level2.h
 double *trsv(long, double *, long, double *, long, void *);
 
+void run(int, int);
+
+int dtrsv_(char *, char *, char *, int *, double *, int *, double *, int *);
+
 int main(int argc, char *argv[])
 {
-  int n, b, i;
-  double ctime, tot_time;
-  double *L, *y, *x;
-
+  int n, b;
   if (argc != 3) {
     printf("usage: ./trsm <n> <b>\n");
     return(-1);
@@ -28,6 +32,15 @@ int main(int argc, char *argv[])
   n = (int)strtol(argv[1], (char **)NULL, 10);
   b = (int)strtol(argv[2], (char **)NULL, 10);
 
+  run(n, b);
+  
+  return 0;
+}
+
+void run(int n, int b) {
+  int i;
+  double *L, *y, *x;
+  double ctime, tot_time;
   // initialization of ithe seed is done once for all
   srand48( (unsigned)time((time_t *) NULL )); 
 
@@ -62,13 +75,20 @@ int main(int argc, char *argv[])
   free(y);
   free(L);
   //  free(x);
-
-  return 0;
 }
 
 
 double *trsm(double *L, double *y, int n, int b) {
-  double *x = malloc(sizeof(double) * n);
+  double *x = (double *) malloc(sizeof(double) * n);
+  
+  // prepare the arguments to give to the dtrsv_ function
+  char *up_lo = "l";
+  char *trans = "T";
+  char *diag = "n";
+  int order = n;
+  int incx = 1;
+
+  dtrsv_(up_lo, trans, diag, &order, L, &n, x, &incx);
   return x;
   // we must print when computed
   // n, ||Lx − y||, and the execution time for TRSV

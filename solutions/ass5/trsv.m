fef06f17501df53bfe6cd14c69af876bdeaa967f
Andrea Crotti <kerny404@gmail.com>
2010-01-08 13:01:53 +0100

    fix cycle on Ltl

--- solutions/ass5/trsv.m
+++ solutions/ass5/trsv.m
@@ -1,15 +1,24 @@
+#!/Applications/Octave.app/Contents/Resources/bin/octave
 ## Taking a triangular lower matrix L as input
 ## we want to find x such that Lx = y
 ## Find out how the triangular linear system actually works
 
 function x = TRSV(L, y, b, alg)
-  ## partitioning
-  ## FIXME: you don't have to cut in a half actually
-  half = length(L) / 2
-  LTL = L(1:half, 1:half)
-  LBL = L(1:half, half+1:length(L))
-  LBR = L(half : length(L(1:)), half+1:length(L))
+  len = length(L);
+  req = len / b;
+  for idx = 1:req
+    step = idx
+    s = idx * b;
+    ## avoiding overflow
+    if s > len
+      s = len
+    endif
 
+    Ltl = L(1:s, 1:s)
+    Lbl = L(s+1:len, 1:s)
+    Lbr = L(s+1:len, s+1:len)
+  endfor
+    
 endfunction
 
 function passed = test_TRSV()
@@ -22,10 +31,13 @@ function passed = test_TRSV()
   for i = m1, m2, m3
     if TRSV(i, y, i, 1) != TRSV(i, y, i, 2)
       printf("something wrong happened")
-      return false
+      return
     endif
   endfor
-  return true
+  return
 endfunction
 
 ## check the accuracy of what we've computed plotting the different results
+y = 1:10
+m1 = tril(rand(10))
+TRSV(m1, y, 1, 0)
